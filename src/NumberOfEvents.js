8338513eff7856512e1c7da5274666a2a91e54a5
Alvaromedia <93778428+Alvaromedia@users.noreply.github.com>
2022-03-13 23:20:38 +0100

    Task 4.8
    Implemented the ErrorAlert subclass in the NumberOfEvents component

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    infoText: null,
   };
 
   handleInputChange = event => {
-    this.setState({
-      numberOfEvents: event.target.value,
-    });
+    if (event.target.value >= 33 || event.target.value <= 0) {
+      this.setState({
+        numberOfEvents: event.target.value,
+        infoText: 'Specified number is not allowed',
+      });
+    } else {
+      this.setState({
+        numberOfEvents: event.target.value,
+        infoText: '',
+      });
+    }
     this.props.updateNumberOfEvents(event.target.value);
   };
-
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.infoText} />
         <input
           type="number"
           className="numberEvents"
